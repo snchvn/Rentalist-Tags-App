cbfa45260449f41585babcdb96ab7fd6b1136af3
San <san@san.is>
2020-08-07 11:51:46 +0100

    env

---
+++ client/build/precache-manifest.d5ec4a91a52b03ac56758c862c22b113.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809ef77a312213903cb96f21711e72f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85438096ff45f28486e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "89b365e0c1f1f61b57f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f85438096ff45f28486e",
+    "url": "/static/js/2.1feb354f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1feb354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b365e0c1f1f61b57f4",
+    "url": "/static/js/main.5bd253dc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
